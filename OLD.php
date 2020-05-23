f77387e836c324505fa2e576fa4bd5785ae6cec6
Huan Tran <huankimtran@gmail.com>
2020-05-23 03:23:11 -0500

    In the middle of desinging th result class, but too sleepy

---
+++ OLD.php
@@ -0,0 +1,17 @@
+<?php
+// This file is the parser for www.oxfordlearnersdictionaries.com
+
+//// Loading the environment for all parser
+require('parser_env.php');
+
+// the url to the website will be parsed by this parser
+define('TargetDict','https://www.oxfordlearnersdictionaries.com/definition/english/');
+
+// Get the word user want to look up
+$word = $_GET[LKW];
+
+//// Parsing code down here 
+
+
+// Return code
+?>
---
+++ parser_env.php
@@ -0,0 +1,7 @@
+<?
+// This file create the common environment for all parsers
+// Well, for now, it just saves constatns
+
+// The GET variable that contains the word needed to be looked up
+define('LKW'.'lookup');
+?>
